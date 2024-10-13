cac3ae0add053ae5cf7d9bd37cbf2afa58a7821a
TaRaKaN41K <FVVMS13.2017@gmail.com>
2024-10-13 03:44:48 +0300

    Change db/Dokerfile

--- db/init.sql
+++ db/init.sql
@@ -7,7 +7,7 @@ BEGIN
       FROM   pg_catalog.pg_roles
       WHERE  rolname = 'postgres') THEN
 
-      CREATE ROLE your_db_user WITH LOGIN PASSWORD 'postgres';
+      CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';
    END IF;
 END
 $do$;
@@ -19,16 +19,16 @@ BEGIN
    IF NOT EXISTS (
       SELECT 1 FROM pg_database
       WHERE datname = 'bot_phon_numbers_and_emails_db') THEN
-      CREATE DATABASE your_db_name;
+      CREATE DATABASE bot_phon_numbers_and_emails_db;
    END IF;
 END
 $do$;
 
 -- Присваиваем права пользователю на созданную базу данных
-GRANT ALL PRIVILEGES ON DATABASE your_db_name TO your_db_user;
+GRANT ALL PRIVILEGES ON DATABASE bot_phon_numbers_and_emails_db TO postgres;
 
 -- Переключаемся на базу данных
-\c your_db_name
+\c bot_phon_numbers_and_emails_db
 
 -- Создание таблицы emails
 CREATE TABLE IF NOT EXISTS emails (
